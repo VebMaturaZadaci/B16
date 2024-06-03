b4f5f775f2931721a1b1d3ceb204c1f2ca5105a4
nik0llaa <nikolastojkovic255@gmail.com>
2024-06-03 14:12:28 +0200

    dodan xml file u folderu

--- B7/obj/Debug/net6.0/B16.AssemblyInfo.cs
+++ B7/obj/Debug/net6.0/B16.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("B16")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3c6a0b214df6ab4585327ebf8f166e8996577dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("B16")]
 [assembly: System.Reflection.AssemblyTitleAttribute("B16")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
